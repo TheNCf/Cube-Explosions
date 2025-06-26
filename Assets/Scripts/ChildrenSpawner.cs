91d2f235cdc47249150730649b1ca4ae49c821fa
TheNCf <nikitadorofeevandreevich2000@gmail.com>
2025-06-27 01:06:16 +0300

    Replaced self-spawning with field dependency

--- Assets/Scripts/ChildrenSpawner.cs
+++ Assets/Scripts/ChildrenSpawner.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 [RequireComponent(typeof(Exploder))]
 public class ChildrenSpawner : MonoBehaviour
 {
+    [SerializeField] private GameObject _childPrefab;
+    [Space(20)]
     [SerializeField, Range(0, 100)] private float _spawnChildrenChance = 100.0f;
     [SerializeField, Min(1)] private int _spawnCountMin = 2;
     [SerializeField, Min(1)] private int _spawnCountMax = 6;
 
     private Exploder _exploder;
+    private float reductionFactor = 2.0f;
 
     private void Awake()
     {
@@ -36,20 +39,19 @@ public class ChildrenSpawner : MonoBehaviour
         {
             float deviationFactor = 0.01f;
             Vector3 randomPositionDeviation = new Vector3(Random.Range(-deviationFactor, deviationFactor), 0, Random.Range(-deviationFactor, deviationFactor));
-            GameObject childCube = Instantiate(gameObject, transform.position + randomPositionDeviation, Quaternion.identity);
+            GameObject childCube = Instantiate(_childPrefab, transform.position + randomPositionDeviation, Quaternion.identity);
 
             Rigidbody childCubeRigidbody = childCube.GetComponent<Rigidbody>();
             _exploder.Explode(childCubeRigidbody);
 
             ChildrenSpawner childController = childCube.GetComponent<ChildrenSpawner>();
-            childController.ReduceChanceAndSize();
+            childController.SetChanceAndSize(_spawnChildrenChance / reductionFactor, transform.localScale / reductionFactor);
         }
     }
 
-    public void ReduceChanceAndSize()
+    public void SetChanceAndSize(float chance, Vector3 size)
     {
-        float reductionFactor = 2.0f;
-        _spawnChildrenChance /= reductionFactor;
-        transform.localScale /= reductionFactor;
+        _spawnChildrenChance = chance;
+        transform.localScale = size;
     }
 }
